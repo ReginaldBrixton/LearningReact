dda36b2f60f3688fb830b9ac10490990d4c3894f
Reginald Emmanuel Brixton <reginaldbrixton@gmail.com>
2024-10-12 17:30:54 +0000

    Remove Alert, Table, and Tooltip components; update NoRecentActivities and Separator components; and modify tsconfig.json

--- components/ui/no-activity.jsx
+++ components/ui/no-activity.jsx
@@ -20,11 +20,6 @@ export default function NoRecentActivities() {
         It looks like there haven&apos;t been any activities lately. 
         New actions will appear here when they occur.
       </p>
-      
-      {/* <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-200 flex items-center space-x-2 group">
-        <span>Refresh</span>
-        <RefreshCw className="w-4 h-4 group-hover:animate-spin" />
-      </button> */}
     </div>
   );
 }
