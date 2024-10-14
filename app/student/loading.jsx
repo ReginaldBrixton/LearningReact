af22d6924d1c6cb94f22b1ced4d9fd0e30dc7a8c
Reginald Emmanuel Brixton <reginaldbrixton@gmail.com>
2024-10-14 04:44:52 +0000

    Updated some packages

--- app/student/loading.jsx
+++ app/student/loading.jsx
@@ -1,11 +1,5 @@
-"use client"
-
 import { MolecularStructureLoaderComponent } from "@/components/LoadingScreens/molecular-structure-loader"
 
 export default function Loading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <MolecularStructureLoaderComponent />
-    </div>
-  )
+  return <MolecularStructureLoaderComponent />
 }
