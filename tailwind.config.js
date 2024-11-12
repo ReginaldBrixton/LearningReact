3022043b34cf806f5c05cba305e387414b731fce
Reginald Emmanuel Brixton <reginaldbrixton@gmail.com>
2024-11-12 04:19:32 +0000

    "Updated VSCode settings, added autoprefixer, and modified postcss and tailwind configurations"

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   darkMode: ["class"],
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
